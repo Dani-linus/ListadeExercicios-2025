460985d2c991fb3d9256188cd8b391152621538f
Dani-linus <dns40625@gmail.com>
2025-04-06 20:16:14 -0300

    Add project files.

---
+++ ListadeExercicios.Exercicio02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ListadeExercicios.Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region input de dados
+            Console.WriteLine("Digite o raio do cilindro: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            #endregion
+
+            #region processamento de dados
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            #endregion
+
+            #region output de dados
+            Console.WriteLine($"O volume do cilindro é {volume}");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListadeExercicios.Exercicios01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ListadeExercicios.Exercicios01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region input de dados
+            Console.WriteLine("Digite o comprimento da caixa:" );
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite o tamanho da largura:" );
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura da caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processamento de dados
+            decimal volume_caixa = comprimento * largura * altura;
+            #endregion
+
+            #region output dos dados
+            Console.WriteLine($"O volume da caixa é {volume_caixa}.");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
